f43993ace621e08d58c775b7e968195234037eac
Kuronekoo <kuroneko.su@outlook.com>
2018-12-11 17:01:46 +0800

    int

---
+++ README.md
@@ -0,0 +1,4 @@
+# rabbit-mq-demo项目
+
+
+
---
+++ src/main/java/cn/kuroneko/demo/RabbitApp.java
@@ -0,0 +1,11 @@
+package cn.kuroneko.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitApp.class,args);
+    }
+}
